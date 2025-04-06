7ed1dd7ff5225067b68190654c0aaf7fcec0c0f9
Arcade-helper <arcadehelper1418@gmail.com>
2025-04-06 20:21:39 +0530

    Create VPC Networking Fundamentals.sh

---
+++ VPC Networking Fundamentals.sh
@@ -0,0 +1,16 @@
+export ZONE1=
+
+export ZONE2=
+
+
+gcloud compute networks create mynetwork --subnet-mode=auto
+gcloud compute instances create mynet-us-vm \
+--zone=$ZONE1 \
+--machine-type=e2-micro \
+--tags=ssh,http,rules \
+--network=mynetwork
+gcloud compute instances create mynet-second-vm \
+--zone=$ZONE1 \
+--machine-type=e2-micro \
+--tags=ssh,http,rules \
+--network=mynetwork
