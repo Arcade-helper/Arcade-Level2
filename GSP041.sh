cdbfbd9859e99649c4b2665bb3d61fa1f3802f83
Arcade-helper <arcadehelper1418@gmail.com>
2025-04-09 21:42:25 +0530

    Update GSP041.sh

--- GSP041.sh
+++ GSP041.sh
@@ -1,5 +1,5 @@
-export ZONE= [Copy it from Lab Instructions]
-export STATIC_IP= [Copy it from Lab Instructions]
+export ZONE=[Copy it from Lab Instructions]
+export STATIC_IP=[Copy it from Lab Instructions]
 
 curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Internal%20Load%20Balancer/gsp041.sh
 
